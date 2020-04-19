25fd184be8d531465af8152ed0be1d69a613ab60
joan-w <wamuyujoan4@gmail.com>
2020-04-20 00:20:07 +0300

    Creating a save_credential method that is called on a credential object and appends it to the credentials_list list

--- password.py
+++ password.py
@@ -2,17 +2,19 @@ class User:
     '''
     A class that generates new instaces of passwords
     '''
-
+    
     credentials_list = [] #Empty credentials list
 
-def __init__(self,account_name,account_password):
-    '''
-    __init__ method that helps us define proprties for our objects.
-    
-    Args:
-    account_name: New account's name.
-    password: New account's password.
-    '''
+    def __init__(self,account_name,account_password):
+        
+        #docstring removed for simplicity
+        
+        self.account_name = account_name
+        self.password = account_password
 
-    self.account_name = account_name
-    self.account_password = password
+    def save_credential(self):
+        '''
+        save_credential method saves credential objects into credentials list
+        '''
+
+        User.credentials_list.append(self)
