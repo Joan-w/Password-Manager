bf62acd15698835747eef2300cba6d6dcc52af45
joan-w <wamuyujoan4@gmail.com>
2020-04-20 00:06:12 +0300

    Importing a unittest module and the user class and creating a testCredentials class that inherits unittest.TestCase

---
+++ password_test.py
@@ -0,0 +1,10 @@
+import unittest #Importing the unittest module
+from password import User #Importing the User class
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines the cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
