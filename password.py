de07cb2171b322bb101927b9cd1ecf856106b3c6
joan-w <wamuyujoan4@gmail.com>
2020-04-19 23:33:30 +0300

    The first commit

---
+++ password.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    A class that generates new instaces of passwords
+    '''
+
+    pass
+
+def __init__(self,account_name,password):
+    '''
+    __init__ method that helps us define proprties for our objects.
+    
+    
+    '''
