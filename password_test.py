964a20de9406cae51f31bdf2205d9f67f2ca5f94
joan-w <wamuyujoan4@gmail.com>
2020-04-20 00:12:20 +0300

    Adding a setUp() method that allows one to define instructions that will be executed before each test method and instructing it to create a new instance of User class before each test method declared and soring it in the test class self.new_credential

--- password_test.py
+++ password_test.py
@@ -8,3 +8,21 @@ class TestCredentials(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+
+        self.new_credential = User("Instagram", "12345") #Create a new credential
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credential.account_name,"Instagram")
+        self.assertEqual(self.new_credential.password,"12345")
+
+if __name__ = "__main__"
+unittest.main()
