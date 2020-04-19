ba2ea61fc55bc32fec4ef88d093594f84ce1ad81
joan-w <wamuyujoan4@gmail.com>
2020-04-19 23:36:10 +0300

    Creating a user class and using the init method to create new instances

--- password.py
+++ password.py
@@ -9,5 +9,7 @@ def __init__(self,account_name,password):
     '''
     __init__ method that helps us define proprties for our objects.
     
-    
+    Args:
+    account_name: New account's name.
+    password: New account's password.
     '''
