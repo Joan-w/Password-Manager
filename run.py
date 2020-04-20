1d8173c55ccbfda31c84dfd9b39b3eea6f133288
joan-w <wamuyujoan4@gmail.com>
2020-04-20 19:48:08 +0300

    Writing a function that takes in a credential object and then calls the delete_credential method on the credential object and deleting it from the credential_list

--- run.py
+++ run.py
@@ -14,3 +14,9 @@ def save_credentials(credential):
     Function to save a credential
     '''
     credential.save_credential()
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credetial()
