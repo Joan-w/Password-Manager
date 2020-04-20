aa8fe51b1c0a031cbab7fac0e75b879ed2ccb966
joan-w <wamuyujoan4@gmail.com>
2020-04-20 19:39:44 +0300

    Correcting th test that allows one to copy the password to the clipboard

--- password.py
+++ password.py
@@ -1,34 +1,55 @@
+import pyperclip
+
 class User:
     '''
     A class that generates new instaces of passwords
     '''
     
-    credentials_list = [] #Empty credentials list
+    credential_list = [] #Empty credentials list
 
     def __init__(self,account_name,account_password):
         
         #docstring removed for simplicity
         
         self.account_name = account_name
-        self.password = account_password
+        self.account_password = account_password
 
     def save_credential(self):
         '''
         save_credential method saves credential objects into credentials list
         '''
 
-        User.credentials_list.append(self)
+        User.credential_list.append(self)
 
     def delete_credential(self):
         '''
         delete_credential method deletes credentials from the crdentials_list
         '''
 
-        User.credentials_list.remove(self)
+        User.credential_list.remove(self)
 
     @classmethod
     def display_credentials(cls):
         '''
         method that returns the credentials list
         '''
-        return cls.credentials_list
+        return cls.credential_list
+
+    @classmethod
+    def find_by_account_name(cls,account_name):
+        '''
+        Method that takes in an account name and returns a credential that matches that account name
+        Args:
+            account_name: Account name to search for
+        Returns:
+            A credential that matches the account name.
+        '''
+
+        for credential in cls.credential_list:
+            if credential.account_name == account_name:
+                return credential
+
+    @classmethod
+    def copy_password(cls,account_name):
+        password_found = User.find_by_account_name(account_name)
+        pyperclip.copy(password_found.account_password)
