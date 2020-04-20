af213205c0a78d8a208241e83b8036dad5aeed9a
joan-w <wamuyujoan4@gmail.com>
2020-04-20 19:44:39 +0300

    Writing a function that creates a new credential

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3.6
 from password import User
+
+def create_credential(acc_name,acc_password):
+    '''
+    Function to create a new credential
+    '''
+
+    new_credential = User(acc_name,acc_password)
+    return new_credential
